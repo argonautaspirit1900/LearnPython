a593aba605f7e99a6256679500b124786de21457
gjergjk71 <gjergjk71@gmail.com>
2019-01-03 17:07:41 +0100

    Created solve_challenge view

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Challenge
 
 # Create your views here.
+
+def solve_challenge(request,pk):
+	challenge = get_object_or_404(Challenge,pk=pk)
+	context = {"challenge":challenge}
+	return render(request,"challenges/solve_challenge.html",context)
