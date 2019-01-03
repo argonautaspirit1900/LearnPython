7abac0743f15afd3a7b4545ea7d282d881ee092f
gjergjk71 <gjergjk71@gmail.com>
2019-01-03 16:36:21 +0100

    Defined ASGI_APPLICATION var,added challenges/channels to INSTALLED_APPS

--- LearnPython/settings.py
+++ LearnPython/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
+
+ASGI_APPLICATION = "LearnPython.routing.application"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
