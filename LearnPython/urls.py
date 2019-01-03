96860784d2c60152fb3a4b3574d7c1ab1119c08b
gjergjk71 <gjergjk71@gmail.com>
2019-01-03 21:33:34 +0100

    Included api.urls to urlpatterns

--- LearnPython/urls.py
+++ LearnPython/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('challenges/',include("challenges.urls")),
+    path('api/',include("api.urls")),
 ]
