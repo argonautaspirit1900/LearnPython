580270153349aac7cf6372d3cf45063e37a177f8
gjergjk71 <gjergjk71@gmail.com>
2019-01-03 21:33:49 +0100

    Created get_output path

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
+from .views import *
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -8,5 +9,6 @@ router.register(r'challenges',ChallengeViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^get_output/',GetOutput.as_view(),name="get_output")
 ]
