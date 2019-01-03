f77b60d26dd09aeac3e9cf9b73320951a0e9be60
gjergjk71 <gjergjk71@gmail.com>
2019-01-03 17:06:52 +0100

    Created Category/Challenge models

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=300)
+
+class Challenge(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	category = models.ForeignKey(Category,on_delete=models.CASCADE)
+	title = models.CharField(max_length=500)
+	description = models.TextField()
+	expected_output = models.TextField()
